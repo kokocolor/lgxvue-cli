a174e2ce9ab8623de825c22cbb61b452c4e7e789
kokocolor <2568936548@qq.com>
2017-05-12 18:03:44 +0800

    lgx vue-cli项目

---
+++ src/module/detail/De.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+
+    <img src="./images/detail.jpg">
+    <div >我的详情</div>
+    <br/>
+      <p v-for="n in pageCount">
+                    <a href="javascripit:void(0)" v-bind:class="activeNumber === n + 1 ? 'active' : ''" v-on:click="greet">{{ n + 1 }}</a>
+                </p>
+    <wo></wo>
+  </div>
+</template>
+
+<script>
+  import 'common/css/reset.css';
+  import wo from 'components/Hello/Wo'
+
+  export default {
+    name: 'app', 
+     data() {
+        return {
+           activeNumber: 2,
+           pageCount: 10
+        }
+       },
+      methods: {
+              greet: function() {
+                  // // 方法内 `this` 指向 vm
+                  alert(this.activeNumber)
+              },
+              say: function(msg) {
+                  alert(msg)
+              }
+          },
+    components: {
+      wo
+    }
+   
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .active{
+  color:green;
+  }
+</style>
---
+++ src/module/detail/detail.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import des from './De'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<des/>',
+  components: { des },
+
+})
